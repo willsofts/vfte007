d3801890924c340e601452d838c153a0e0f7ddd0
tassun <32536248+tassun@users.noreply.github.com>
2024-10-17 14:07:00 +0700

    initial project

---
+++ src/AppVfte007.vue
@@ -0,0 +1,136 @@
+<!-- App.vue -->
+<template>
+  <div id="fswaitlayer" class="fa fa-spinner fa-spin"></div>
+  <div class="pt-page pt-page-current pt-page-controller search-pager">
+    <PageHeader ref="pageHeader" :labels="labels" pid="vfte007" version="1.0.0" showLanguage="true" @language-changed="changeLanguage" :multiLanguages="multiLanguages" />
+    <SearchForm ref="searchForm" :labels="labels" :dataCategory="dataCategory" @data-select="dataSelected" />
+  </div>
+  <teleport to="#modaldialog">
+    <EntryForm ref="entryForm" :labels="labels" :dataCategory="dataCategory" @data-updated="dataUpdated" />
+  </teleport>
+</template>
+<script>
+import { ref } from 'vue';
+import $ from "jquery";
+import { PageHeader } from '@willsofts/will-control';
+import SearchForm from '@/components/SearchForm.vue';
+import EntryForm from '@/components/EntryForm.vue';
+import { getLabelModel, getMultiLanguagesModel } from "@willsofts/will-app";
+import { DEFAULT_CONTENT_TYPE, getDefaultLanguage, setDefaultLanguage, getApiUrl } from "@willsofts/will-app";
+import { startApplication, serializeParameters } from "@willsofts/will-app";
+
+export default {
+  components: {
+    PageHeader, SearchForm, EntryForm
+  },
+  setup() {
+    const dataChunk = {};
+    const dataCategory = {
+      tcompbranch: [],
+      trole: [],
+      tgroup: [],
+      tkuserstatus: [{id: "A", text: "Activated"},{id: "C", text: "Closed"}, {id: "P", text: "Pending"}],
+    };
+    let labels = ref(getLabelModel());
+    let alreadyLoading = ref(false);
+    const multiLanguages = ref(getMultiLanguagesModel());
+    return { multiLanguages, labels, dataCategory, dataChunk, alreadyLoading };
+  },
+  mounted() {
+    console.log("App: mounted ...");
+    this.$nextTick(async () => {
+      //ensure ui completed then invoke startApplication 
+      startApplication("vfte007",(data) => {
+        this.multiLanguages = getMultiLanguagesModel();
+        this.messagingHandler(data);
+        this.loadDataCategories(!this.alreadyLoading,() => {
+          this.$refs.pageHeader.changeLanguage(getDefaultLanguage());
+        });
+      });
+    });
+  },
+  methods: {
+    messagingHandler(data) {
+      console.log("messagingHandler: data",data); 
+    },
+    changeLanguage(lang) {
+      setDefaultLanguage(lang);
+      let labelModel = getLabelModel(lang);
+      this.labels = labelModel;
+      this.resetDataCategories(lang);
+    },
+    loadDataCategories(loading,callback) {
+      console.log("loadDataCategories: loading",loading);
+      if(!loading) return;
+      let jsondata = {names: ["tcompbranch", "trole", "tgroup", "tkuserstatus"]};
+      let formdata = serializeParameters(jsondata);
+      $.ajax({
+        url: getApiUrl()+"/api/category/lists",
+        data: formdata.jsondata,
+        headers : formdata.headers,
+        type: "POST",
+        dataType: "json",
+        contentType: DEFAULT_CONTENT_TYPE,
+        error : function(transport,status,errorThrown) {
+          console.error("loadDataCategories: error: status",status,"errorThrown",errorThrown);
+        },
+        success: (data) => {
+          this.alreadyLoading = true;
+          console.log("loadDataCategories: success",data);
+          if(data.body) {
+            for(let item of data.body) {
+              if(item.category && item.resultset && item.resultset.rows) {
+                this.dataChunk[item.category] = item.resultset.rows;
+              }
+            }
+            console.log("data chunk",this.dataChunk);
+            this.resetDataCategories();
+            if(callback) callback();
+          }
+        }
+      });	
+    },
+    resetDataCategories(lang) {
+      if(!lang) lang = getDefaultLanguage();
+      if(!lang || lang.trim().length==0) lang = "EN";
+      let tcompbranch;
+      let trole;
+      let tgroup;
+      let tkuserstatus;
+      let tk_category = this.dataChunk["tcompbranch"];
+      if(tk_category) {
+        tcompbranch = tk_category.map((item) => { return { id: item.branch, text: "TH"==lang?item.nameth:item.nameen } });
+      }
+      tk_category = this.dataChunk["trole"];
+      if(tk_category) {
+        trole = tk_category.map((item) => { return { id: item.roleid, text: "TH"==lang?item.nameth:item.nameen } });
+      }
+      tk_category = this.dataChunk["tgroup"];
+      if(tk_category) {
+        tgroup = tk_category.map((item) => { return { id: item.groupname, text: "TH"==lang?item.nameth:item.nameen } });
+      }
+      tk_category = this.dataChunk["tkuserstatus"];
+      if(tk_category) {
+        tkuserstatus = tk_category.map((item) => { return { id: item.typeid, text: "TH"==lang?item.nameth:item.nameen } });
+      }
+      if(tcompbranch) this.dataCategory.tcompbranch = tcompbranch;
+      if(trole) this.dataCategory.trole = trole;
+      if(tgroup) this.dataCategory.tgroup = tgroup;
+      if(tkuserstatus) this.dataCategory.tkuserstatus = tkuserstatus;
+    },
+    dataSelected(item,action) {
+      //listen action from search form
+      console.log("App: dataSelected",item,"action",action);
+      if("edit"==action) {
+        this.$refs.entryForm.retrieveRecord({userid: item.userid});
+      }
+    },
+    dataUpdated(data,response) {
+      //listen action from entry form when after updated
+      console.log("App: record updated");
+      console.log("data",data,"response",response);
+      this.$refs.searchForm.search();
+    },
+  }
+};
+</script>
---
+++ src/components/EntryForm.vue
@@ -0,0 +1,386 @@
+<template>
+  <DialogForm ref="dialogForm">
+    <template v-slot:header>
+      <h4 class="modal-title">{{ labels.title_edit }}</h4>
+    </template>
+    <template v-slot:default>
+        <div class="row row-height">
+          <div class="col-height col-md-8">
+            <label for="username">{{ labels.username_label }}</label>
+            <div class="input-group has-validation" :class="{'has-error': v$.username.$error}">
+              <InputMask ref="username" v-model="localData.username" id="username" picture="(50)x" /> 
+              <label class="required">*</label>
+            </div>
+            <span v-if="v$.username.$error" class="has-error">{{ v$.username.$errors[0].$message }}</span>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-4">
+              <input type="text" v-model="localData.usertname" class="form-control input-md" readonly />
+						</div>
+						<div class="col-height col-md-4">
+              <input type="text" v-model="localData.usertsurname" class="form-control input-md" readonly />
+						</div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-8">
+            <label class="control-label">{{ labels.email_label }}</label>
+            <input type="text" v-model="localData.email" class="form-control input-md" readonly />
+					</div>
+        </div>
+        <div class="row row-height">
+          <div class="col-height col-md-4">
+            <label for="status">{{ labels.status_label }}</label>
+              <select ref="status" v-model="localData.status" class="form-control input-md">
+                <option v-for="item in dataCategory.tkuserstatus" :key="item.id" :value="item.id">{{item.text}}</option>
+              </select>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div id="accessalls" class="col-md-12 col-height">
+            <fieldset id="accessallsheaderinfo">
+              <legend id="accessalls_legend">
+                <label class="control-label">{{ labels.accessalls_sectionlabel }}</label>
+              </legend>
+            </fieldset>							
+            <div id="branchflaglayer" class="table-layer-class">
+              <div class="row row-height">
+                <div class="col-height col-md-8">
+                  <div class="checkbox form-check">
+                    <input ref="branchflag" type="checkbox" true-value="1" false-value="0" id="branchflag" v-model="localData.branchflag" class="form-control input-md form-check-input" />
+                    <label for="branchflag" class="control-label form-check-label">{{ labels.branchflag_label }}</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div id="accessbranches" class="col-md-12 col-height">
+            <fieldset id="accessbranchesfieldset" :disabled="!isAccessBranch">
+              <div id="accessbrancheslayer" class="table-layer-class">
+                <div v-for="(branch, index) in branchLists()" :key="index" class="row row-heighter">
+                  <div v-for="item in branch" :key="item.id" class="col-height col-md-4">
+                    <div class="checkbox form-check form-group">
+                      <label class="form-check-label">
+                        <input type="checkbox" :value="item.id" v-model="localData.userbranches" class="form-control input-md form-check-input"/>
+                        <span class="span-label">{{item.text}}</span>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </fieldset>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div id="accessroles" class="col-md-12 col-height">
+            <fieldset id="accessrolesheaderinfo">
+              <legend id="accessrole_legend">
+                <label id="accessroles_sectionlabel" class="control-label">{{ labels.accessroles_sectionlabel }}</label>
+              </legend>
+            </fieldset>									
+            <div id="accessroleslayer" class="table-layer-class">				
+              <div v-for="(role, index) in roleLists()" :key="index" class="row row-heighter">
+                <div v-for="item in role" :key="item.id" class="col-height col-md-4">
+                  <div class="checkbox form-check form-group">
+                    <label class="form-check-label">
+                      <input type="checkbox" :value="item.id" v-model="localData.userroles" class="form-control input-md form-check-input"/>
+                      <span class="span-label">{{item.text}}</span>
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row row-height">
+          <div id="accessgroups" class="col-md-12 col-height">
+            <fieldset id="accessgroupsheaderinfo">
+              <legend id="accessgroups_legend">
+                <label id="accessgroups_sectionlabel" class="control-label">{{ labels.accessgroups_sectionlabel }}</label>
+              </legend>
+            </fieldset>									
+            <div id="accessgroupslayer" class="table-layer-class">
+              <div v-for="(group, index) in groupLists()" :key="index" class="row row-heighter">
+                <div v-for="item in group" :key="item.id" class="col-height col-md-4">
+                  <div class="checkbox form-check form-group">
+                    <label class="form-check-label">
+                      <input type="checkbox" :value="item.id" v-model="localData.usergroups" class="form-control input-md form-check-input"/>
+                      <span class="span-label">{{item.text}}</span>
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </template>
+    <template v-slot:footer>
+      <div class="col-md-3 pull-left" v-if="isRegisteredFactor">
+        <button ref="resetfactorbutton" id="resetfactorbutton" class="btn btn-dark btn-sm" @click="resetFactorClick" v-if="showFactor"><em class="fa fa-undo fa-btn-icon"></em>{{ labels.reset_factor_button }}</button>
+			</div>
+      <button ref="updatebutton" id="updatebutton" class="btn btn-dark btn-sm" @click="updateClick"><em class="fa fa-save fa-btn-icon"></em>{{ labels.update_button }}</button>
+      <button class="btn btn-dark btn-sm" data-dismiss="modal"><em class="fa fa-close fa-btn-icon"></em>{{ labels.cancel_button }}</button>
+    </template>
+  </DialogForm>
+</template>
+<style>
+#branchflag_label { color: navy; }
+#resetfactorbutton { min-width: 120px; }
+#accessallsheaderinfo { min-height: 35px; padding-left: 5px; }
+#accessalls_legend { margin-bottom: 0px; }
+#branchflaglayer { padding-left: 5px; }
+#accessbrancheslayer { padding-left: 40px; }
+#accessrolesheaderinfo { min-height: 35px; padding-left: 5px; }
+#accessrole_legend { margin-bottom: 0px; }
+#accessroleslayer { padding-left: 5px; }
+#accessgroupsheaderinfo { min-height: 35px; padding-left: 5px; }
+#accessgroups_legend { margin-bottom: 0px; }
+#accessgroupslayer { padding-left: 5px; }
+span.span-label { padding-left: 10px; }
+</style>
+<script>
+import { ref, computed, watch } from 'vue';
+import { useVuelidate } from '@vuelidate/core';
+import { required, helpers } from '@vuelidate/validators';
+import $ from "jquery";
+import { DEFAULT_CONTENT_TYPE, getApiUrl, disableControls }  from '@willsofts/will-app';
+import { startWaiting, stopWaiting, submitFailure, detectErrorResponse }  from '@willsofts/will-app';
+import { confirmUpdate, successbox, serializeParameters, alertmsg, confirmDialogBox } from '@willsofts/will-app';
+import { InputMask } from '@willsofts/will-control';
+import DialogForm from './DialogForm.vue';
+
+const APP_URL = "/api/sfte007";
+const defaultData = {
+  site: "",
+  userid: "",
+  username: "",
+  usertname: "",
+  usertsurname: "",
+  userename: "",
+  useresurname: "",
+  email: "",
+  status: "A",
+  siteflag: "",
+  branchflag: "",
+  displayname: "",
+  userbranch: "",
+  sitedesc: "",
+  factorid: "",
+  factorflag: "",
+  usersites: [],
+  userbranches: [],
+  userroles: [],
+  usergroups: [],
+};
+
+export default {
+  components: {
+    DialogForm, InputMask
+  },
+  props: {
+    modes: Object,
+    labels: Object,
+    dataCategory: Object
+  },
+  emits: ["data-updated","data-reseted"],
+  setup(props) {
+    const localData = ref({ ...defaultData }); 
+    const reqalert = ref(props.labels.empty_alert);
+    const requiredMessage = () => {
+      return helpers.withMessage(reqalert, required);
+    }
+    const validateRules = computed(() => { 
+      return {
+        username: { required: requiredMessage() },
+      } 
+    });
+    const showFactor = ref(true);
+    const v$ = useVuelidate(validateRules, localData, { $lazy: true, $autoDirty: true });
+    return { v$, localData, reqalert, showFactor };
+  },
+  created() {
+    watch(this.$props, (newProps) => {      
+      this.reqalert = newProps.labels.empty_alert;
+    });
+  },
+  computed: {
+    isRegisteredFactor() {
+      return this.localData.factorflag == "1";
+    },
+    isAccessBranch() {
+      return this.localData.branchflag == "1";
+    }
+  },
+  mounted() {
+    this.$nextTick(function () {
+      $("#modaldialog_layer").find(".modal-dialog").draggable();
+    });
+  },
+  methods: {
+    buildLists(category,chunkSize=3) {
+      let results = [];
+      let tcategory = this.$props.dataCategory[category];
+      if(tcategory) {
+        for (let i = 0; i < tcategory.length; i += chunkSize) {
+          results.push(tcategory.slice(i, i + chunkSize));
+        }
+      }
+      return results;
+    },
+    branchLists() {
+      return this.buildLists("tcompbranch");
+    },
+    roleLists() {
+      return this.buildLists("trole");
+    },
+    groupLists() {
+      return this.buildLists("tgroup");
+    },
+    reset(newData) {
+      if(newData) this.localData = {...newData};
+    },
+    async updateClick() {
+      console.log("click: update",this.isAccessBranch);
+      disableControls($("#updatebutton"));
+      let valid = await this.validateForm();
+      if(!valid) return;
+      this.startUpdateRecord();
+    },
+    async validateForm(focusing=true) {
+      const valid = await this.v$.$validate();
+      console.log("validate form: valid",valid);
+      console.log("error:",this.v$.$errors);
+      if(!valid) {
+        if(focusing) {
+          this.focusFirstError();
+        }
+        return false;
+      }
+      return true;
+    },
+    focusFirstError() {
+      if(this.v$.$errors && this.v$.$errors.length > 0) {
+        let input = this.v$.$errors[0].$property;
+        let el = this.$refs[input];
+        if(el) el.focus(); //if using ref
+        else $("#"+input).trigger("focus"); //if using id
+      }
+    },
+    showDialog() {
+      //$("#modaldialog_layer").modal("show");
+      $(this.$refs.dialogForm.$el).modal("show");
+    },  
+    hideDialog() {
+      //$("#modaldialog_layer").modal("hide");
+      $(this.$refs.dialogForm.$el).modal("hide");
+    },
+    resetRecord() {
+      //reset to default data 
+      this.reset(defaultData);
+      //reset validator
+      this.v$.$reset();
+      //enable key field
+      this.disabledKeyField = false;
+    },
+    startUpdateRecord() {
+      confirmUpdate(() => {
+        if(!this.isAccessBranch) this.localData.userbranches = []; 
+        this.updateRecord(this.localData);
+      });
+    },
+    updateRecord(dataRecord) {
+        let jsondata = {ajax: true};
+        let formdata = serializeParameters(jsondata,dataRecord);
+        startWaiting();
+        $.ajax({
+          url: getApiUrl()+APP_URL+"/update",
+          data: formdata.jsondata,
+          headers : formdata.headers,
+          type: "POST",
+          dataType: "json",
+          contentType: DEFAULT_CONTENT_TYPE,
+          error : function(transport,status,errorThrown) {
+            console.error("error: status",status,"errorThrown",errorThrown);
+            submitFailure(transport,status,errorThrown);
+          },
+          success: (data) => {
+            stopWaiting();
+            console.log("success",data);
+            if(detectErrorResponse(data)) return;
+            successbox(() => {
+              this.hideDialog();
+              this.$emit('data-updated',dataRecord,data);
+            });
+          }
+      });
+    },
+    retrieveRecord(dataKeys) {
+      let jsondata = {ajax: true};
+      let formdata = serializeParameters(jsondata,dataKeys);
+      startWaiting();
+      $.ajax({
+        url: getApiUrl()+APP_URL+"/retrieve",
+        data: formdata.jsondata,
+        headers : formdata.headers,
+        type: "POST",
+        dataType: "json",
+        contentType: DEFAULT_CONTENT_TYPE,
+        error : function(transport,status,errorThrown) {
+          console.error("retrieveRecord: error: status",status,"errorThrown",errorThrown);
+          submitFailure(transport,status,errorThrown);
+        },
+        success: (data) => {
+          stopWaiting();
+          console.log("retrieveRecord: success",data);
+          if(data.body.dataset) {
+            this.reset(data.body.dataset);
+            this.v$.$reset();
+            this.disabledKeyField = true;
+            this.showDialog();
+          }
+        }
+      });	
+    },
+    resetFactorClick() {
+      disableControls($("#resetfactorbutton"));
+      this.startResetFactor();
+    },
+    startResetFactor() {
+      this.confirmResetFactor(() => { 
+        this.resetFactor({userid: this.localData.userid, factorid: this.localData.factorid});
+      });
+    },
+    confirmResetFactor(okFn, cancelFn,  width, height) {
+      if(!confirmDialogBox("QS0021",null,"Do you want to reset two factor authentication?",okFn,cancelFn,width,height)) return false;
+      return true;
+    },
+    resetFactor(dataKeys) {
+      let jsondata = {ajax: true};
+      let formdata = serializeParameters(jsondata,dataKeys);
+      startWaiting();
+      $.ajax({
+        url: getApiUrl()+APP_URL+"/resetfactor",
+        data: formdata.jsondata,
+        headers: formdata.headers,
+        type: "POST",
+        dataType: "json",
+        contentType: DEFAULT_CONTENT_TYPE,
+        error : function(transport,status,errorThrown) {
+          console.error("resetfactor: error: status",status,"errorThrown",errorThrown); 
+          submitFailure(transport,status,errorThrown);  
+        },
+        success: (data) => { 
+          stopWaiting();
+          console.log("resetFactor: success",data);
+          this.showFactor = false;
+          alertmsg("QS0202","Reset Two Factor Success",undefined,() => {
+            this.$emit('data-reseted',dataKeys,data);
+          });
+        }
+      });	
+    },
+  }
+};
+</script>
