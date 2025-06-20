dca53c6d1e5912af50d3e2aca941c65a282962e6
tassan_oro <32536248+tassun@users.noreply.github.com>
2025-06-20 16:51:43 +0700

    support language change from menu

--- src/AppVfte007.vue
+++ src/AppVfte007.vue
@@ -43,11 +43,19 @@ export default {
     this.$nextTick(async () => {
       //ensure ui completed then invoke startApplication 
       startApplication("vfte007",(data) => {
-        this.multiLanguages = getMultiLanguagesModel();
-        this.messagingHandler(data);
-        this.loadDataCategories(!this.alreadyLoading,() => {
-          this.$refs.pageHeader.changeLanguage(getDefaultLanguage());
-        });
+        console.log("vueapp: message",data);
+        if(data.type=="language") {
+          let lang = data.language;
+          if(lang) {
+            this.changeLanguage(lang);
+          }
+        } else {
+          this.multiLanguages = getMultiLanguagesModel();
+          this.messagingHandler(data);
+          this.loadDataCategories(!this.alreadyLoading,() => {
+            this.$refs.pageHeader.changeLanguage(getDefaultLanguage());
+          });
+        }
       });
     });
   },
